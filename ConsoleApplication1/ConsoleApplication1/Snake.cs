b890f6d107769f93ea45721161b0e59b1ab8b2a2
Georgy <yegorf2010@gmail.com>
2017-01-15 18:59:23 +0500

    класс змейка

---
+++ ConsoleApplication1/ConsoleApplication1/Snake.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1 {
+    class Snake:Figure {
+        public Snake(Point tail,int length, Direction direction) {
+            plist=new List<Point>();
+            for (int i = 0; i< length; i++) {
+                var p = new Point(tail);
+                p.Move(i, direction);
+                plist.Add(p);
+            }
+        }
+    }
+}
